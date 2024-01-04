46e468abffc0b1e841f32bc9471e2b61afa0c622
Çağrıhan Aydın <cagrihanaydin@gmail.com>
2024-01-04 15:09:10 +0300

    Add project files.

---
+++ MagicKeys/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int minVal = int.MinValue;
+int maxVal = int.MaxValue;
+Console.WriteLine($"Min integer deger:{minVal}, Max Integer deger:{maxVal}");
