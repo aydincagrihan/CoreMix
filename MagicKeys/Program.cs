81b92a43247b652fc3526cd0b23d1a50d56637d9
Çağrıhan Aydın <cagrihanaydin@gmail.com>
2024-01-04 15:53:20 +0300

    2

--- MagicKeys/Program.cs
+++ MagicKeys/Program.cs
@@ -1,9 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int minVal = int.MinValue;
-int maxVal = int.MaxValue;
-//overflow hatası vermesin diye default olarak false oluyor ve min >max max>min oluyor
-minVal--;
-maxVal++;
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//int minVal = int.MinValue;
+//int maxVal = int.MaxValue;
+////overflow hatası vermesin diye default olarak false oluyor ve min >max max>min oluyor
+//minVal--;
+//maxVal++;
 
-Console.WriteLine($"Min integer deger:{minVal}, Max Integer deger:{maxVal}");
+//Console.WriteLine($"Min integer deger:{minVal}, Max Integer deger:{maxVal}");
+
+
+using MagicKeys;
+
+//var student=new Student();
+//Console.WriteLine(student);
+
+//Object Initalizer 
+var numbers = new List<int>()
+{
+    {1},
+    {2},
+    {3}
+
+};
+numbers.Add(4);
+foreach (var number in numbers)
+{
+    Console.WriteLine(number);
+}
+
---
+++ MagicKeys/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicKeys
+{
+    [DebuggerDisplay("")]
+    public class Student
+    {
+        public string FirstName { get; set; } = "Çağrıhan Aydın";
+        public string LastName { get; set; } = "Tarım";
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+
+    }
+}
